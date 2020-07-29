951d3966e359b25a5773c1a359f9f20bc09935ea
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-07-29 10:50:16 -0300

    starting small project with strapi

---
+++ config/database.js
@@ -0,0 +1,27 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi123'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {
+        debug: true,
+        pool: {
+          min: 0,
+          max: 10,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000
+        }
+      }
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9211050d-67d8-4694-8952-b9e826d3df8f'
+};
